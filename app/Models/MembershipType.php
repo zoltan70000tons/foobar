c5bc854d31aea2d1e94b783251de0e0c9826cbad
blift70k <jacek@70000tons.com>
2024-08-27 10:51:09 +0200

    Typo fix

--- app/Models/MembershipType.php
+++ app/Models/MembershipType.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class MembershipType extends Model
 {
-    protected $table = 'membership_types';
-    protected $fillable = ['name','name', 'stamp_image', 'booking_number_requirement', 'discount_value'];
+  protected $table = 'membership_types';
+  protected $fillable = ['name', 'stamp_image', 'booking_number_requirement', 'discount_value'];
 }
