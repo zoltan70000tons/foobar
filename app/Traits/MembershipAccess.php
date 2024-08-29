2a709d3b9d82a3f3c375a68f50a1be6496877541
blift70k <jacek@70000tons.com>
2024-08-29 12:04:00 +0200

    test of selling periods with refactor of membership seeder

--- app/Traits/MembershipAccess.php
+++ app/Traits/MembershipAccess.php
@@ -4,6 +4,7 @@ namespace App\Traits;
 
 use Carbon\Carbon;
 use App\Models\PresalePeriod;
+use App\Models\Event;
 use Illuminate\Support\Facades\Log;
 
 trait MembershipAccess
@@ -14,31 +15,38 @@ trait MembershipAccess
    * 
    * @param  \App\Models\MembershipType|null  $membership
    * @param  string|null  $currentDate
+   * @param  int|null  $id
    * @return array
    */
-  public function checkMembershipAccess($membership, $currentDate = null)
+  public function checkMembershipAccess($membership, $currentDate = null, $id = null)
   {
     $currentDate = $currentDate ?: Carbon::now()->format('Y-m-d');
-    $public_sales = '2024-12-01';
+
+    // Get event by id
+    $event = Event::find($id);
 
     // Query the presale periods table to get the relevant dates
     $presalePeriod = PresalePeriod::where('membership_type_id', $membership->id ?? null)->first();
 
-    // If no membership, check if the current date is after the "Everyone" access date
-    if (!$membership && $currentDate < $public_sales) {
+    // If no membership, check status of event
+    if (!$membership && $event->status !== 'public') {
       return [
         'status' => false,
         'message' => __('auth.no_access_to_sales_everyone')
       ];
     }
 
-    // If no membership, but the current date is after the "Everyone" access date
-    if (!$membership && $currentDate >= $public_sales) {
+    // If no membership, but event status is public
+    if (!$membership && $event->status === 'public') {
       return ['status' => true];
     }
 
     // If membership, check if the current date is after the membership access date
-    if ($presalePeriod) {
+    if (
+      $presalePeriod &&
+      ($event->status === 'pre-sale' || $event->status === 'public') &&
+      $currentDate <= $presalePeriod->start_date
+    ) {
       return [
         'status' => false,
         'message' => __('auth.no_access_to_sales_not_allowed_type', ['name' => $membership->name])
---
+++ database/factories/MembershipFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Membership;
+use App\Models\Customer;
+use App\Models\MembershipType;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Membership>
+ */
+class MembershipFactory extends Factory
+{
+  protected $model = Membership::class;
+
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array
+  {
+    return [
+      'customer_id' => Customer::factory(),
+      'membership_id' => MembershipType::all()->random()->id,
+    ];
+  }
+
+
+  public function forCustomer($customerId)
+  {
+    return $this->state(function (array $attributes) use ($customerId) {
+      return [
+        'customer_id' => $customerId,
+      ];
+    });
+  }
+}
