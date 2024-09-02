c36126a7544718b09c3f41a04e104d940a366c89
blift70k <jacek@70000tons.com>
2024-09-02 17:28:00 +0200

    Selling periods translations and logic change

---
+++ lang/en/event.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+
+  /*
+    |--------------------------------------------------------------------------
+    | Event Language Lines EN
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during events for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+  'no_event_found' => 'No events found',
+
+];
