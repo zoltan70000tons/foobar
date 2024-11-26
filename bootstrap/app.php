5b3f7e7a8ba0991c51b275476fc470014d53f6b2
blift70k <jacek@70000tons.com>
2024-11-26 16:27:20 +0100

    fix price calc

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,11 +20,9 @@ return Application::configure(basePath: dirname(__DIR__))
     $middleware->alias([
       "verified" => \App\Http\Middleware\EnsureEmailIsVerified::class,
       "auth.customer" => \App\Http\Middleware\AuthenticateCustomer::class,
-      "ensure_not_customer" =>
-        \App\Http\Middleware\EnsureUserIsNotCustomer::class,
+      "ensure_not_customer" => \App\Http\Middleware\EnsureUserIsNotCustomer::class,
       "membership_sales" => \App\Http\Middleware\MembershipSales::class,
-      "clear_expired_reservation" =>
-        \App\Http\Middleware\ClearExpiredReservation::class,
+      "clear_expired_reservation" => \App\Http\Middleware\ClearExpiredReservation::class,
     ]);
 
     $middleware->web(
@@ -45,9 +43,7 @@ return Application::configure(basePath: dirname(__DIR__))
     );
     ////////////////////////
     //this is just for testing, remove in production for security reasons
-    $middleware->validateCsrfTokens(
-      except: ["http://localhost:8000/contact/submit"]
-    );
+    //$middleware->validateCsrfTokens(except: ["http://localhost:8000/contact/submit"]);
     ////////////////////////
   })
   ->withExceptions(function (Exceptions $exceptions) {
