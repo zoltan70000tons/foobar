418b0616feda016d61f15604382e9c51d0ec899b
blift70k <jacek@70000tons.com>
2025-01-14 14:42:30 +0100

    telescope

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,10 +1,11 @@
 <?php
 
 return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\AuthServiceProvider::class,
-    App\Providers\HelpersServiceProvider::class,
-    App\Providers\RepositoryServiceProvider::class,
-    Propaganistas\LaravelPhone\PhoneServiceProvider::class,
-    Spatie\Permission\PermissionServiceProvider::class,
+  App\Providers\AppServiceProvider::class,
+  App\Providers\AuthServiceProvider::class,
+  App\Providers\HelpersServiceProvider::class,
+  App\Providers\RepositoryServiceProvider::class,
+  App\Providers\TelescopeServiceProvider::class,
+  Propaganistas\LaravelPhone\PhoneServiceProvider::class,
+  Spatie\Permission\PermissionServiceProvider::class,
 ];
