0b71a1d19e929ac12e7e6fed491d5539b524c6c5
blift70k <jacek@70000tons.com>
2025-02-26 14:01:04 +0100

    test for login via email and surivivor number

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,13 @@ class UserFactory extends Factory
   public function definition(): array
   {
     return [
-      'name' => fake()->name(),
-      'email' => fake()->unique()->safeEmail(),
+      'username' => fake()->userName(),
+      'email' => fake()->unique()->userName() . '@customers.test',
+      'user_activated_at' => now(),
       'email_verified_at' => now(),
-      'password' => static::$password ??= Hash::make('password'),
+      'password' => Hash::make('password'),
       'remember_token' => Str::random(10),
+      'organization_id' => env('ORGANIZATION_ID', 1),
     ];
   }
 
@@ -37,8 +39,10 @@ class UserFactory extends Factory
    */
   public function unverified(): static
   {
-    return $this->state(fn(array $attributes) => [
-      'email_verified_at' => null,
-    ]);
+    return $this->state(
+      fn(array $attributes) => [
+        'email_verified_at' => null,
+      ]
+    );
   }
 }
