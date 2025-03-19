e80d8c4d2d7b9df27225482df01b084afdd50a0f
leonardo <leonardo@70000tons.com>
2025-03-19 14:04:44 -0300

    Update CheckAllowedDomains.php

--- app/Http/Middleware/CheckAllowedDomains.php
+++ app/Http/Middleware/CheckAllowedDomains.php
@@ -11,13 +11,10 @@ class CheckAllowedDomains
     public function handle(Request $request, Closure $next): Response
     {
         $allowedDomains = explode(',', env('NOTIFICATION_ALLOWED_DOMAINS', 'localhost'));
-
-        $origin = parse_url($request->headers->get('origin') ?? '', PHP_URL_HOST);
-
-        if (!in_array($allowedDomains, ['*']) && !in_array($origin = $request->getHost(), $allowedDomains)) {
+        $origin = parse_url($request->headers->get('origin') ?? '', PHP_URL_HOST) ?: $request->getHost();
+        if (!in_array('*', $allowedDomains) && !in_array($origin, $allowedDomains)) {
             return response()->json(['error' => 'Unauthorized domain.'], 403);
         }
-
         return $next($request);
     }
 }
