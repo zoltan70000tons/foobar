0bff454c52cabb5596ca82ccb040292ef5605bd7
leonardo <leonardo@70000tons.com>
2025-03-20 11:54:37 -0300

    Fix error trying to access fullname on an empty seat

--- resources/js/Pages/Bookings/partials/Passengers.tsx
+++ resources/js/Pages/Bookings/partials/Passengers.tsx
@@ -18,6 +18,10 @@ import { deepOrange, deepPurple, red, pink, purple, yellow, lime, brown, grey, b
 import { useSnackbar } from "@/Providers/SnackBarAlertProvider";
 import EditPassengerModal from "./EditPassengerModal";
 import { router } from "@inertiajs/react";
+import { PersonAdd } from "@mui/icons-material";
+import PersonIcon from '@mui/icons-material/Person';
+import Person2Icon from '@mui/icons-material/Person2';
+
 
 type PassengersProps = {
   booking: Booking;
@@ -84,7 +88,7 @@ const Passengers: React.FC<PassengersProps> = ({ booking, editMode, setLoading }
       showSnackbar("Error updating passenger data!", "error");
     } finally {
       setSavingLoading(false);
-     }
+    }
   };
 
   const onDelete = () => {
@@ -133,6 +137,55 @@ const Passengers: React.FC<PassengersProps> = ({ booking, editMode, setLoading }
     setOpenConfirm(false);
   };
 
+  const getAvatar = (passenger) => {
+    console.log(passenger);
+    const iconProps = {
+      sx: {
+        width: 50,
+        height: 50,
+        mr: 2,
+        cursor: "pointer",
+        color: passenger.lead_passenger ? '#ffa726' : getAvatarColor(passenger),
+      },
+      
+    };
+
+    const IconComponent = getAvatarIcon(passenger);
+
+    return <IconComponent {...iconProps} />;
+  };
+
+  const getAvatarColor = (passenger) => {
+    switch (passenger.gender?.toLowerCase()) {
+      case "m":
+        return "#2196F3";
+      case "f":
+        return "#E91E63";
+      default:
+        return "gray";
+    }
+  };
+
+  const getAvatarIcon = (passenger) => {
+
+    if (passenger.empty) return PersonAdd;
+    switch (passenger.gender?.toLowerCase()) {
+      case "m":
+        return PersonIcon;
+      case "f":
+        return Person2Icon;
+      default:
+        return PersonIcon;
+    }
+  };
+
+
+  const getPassengerBgColor = (passenger) => {
+    if (passenger.lead_passenger) return "#B0BEC5";
+    if (passenger.empty) return "#90CAF9";
+    return "#FFF59D";
+  };
+
   return (
     <Box>
       <Typography variant="h5" gutterBottom>
@@ -141,26 +194,22 @@ const Passengers: React.FC<PassengersProps> = ({ booking, editMode, setLoading }
       <Paper variant="outlined" sx={{ p: 2, backgroundColor: "#1c1c1c", mb: 4 }}>
         <Grid container spacing={2} alignItems="center">
           {passengers.map((passenger, index) => (
-            <Grid item xs={12} sm={3} key={passenger.id + passenger.email}>
-              <Box display="flex" alignItems="center">
-                <Avatar
-                  sx={{
-                    width: 50, height: 50, mr: 2, cursor: "pointer", bgcolor: passenger.lead_passenger
-                      ? green[800]
-                      : passenger.empty
-                        ? grey[500]
-                        : colors[index]
-                          ? colors[index][500]
-                          : grey[300]
-                  }}
-                  onClick={() => handleEditPassenger(passenger)}
-                >
-                  {passenger.full_name[0]}
-                </Avatar>
+            <Grid item xs={12} sm={6} md={3} key={passenger.id + passenger.email}>
+              <Box display="flex" alignItems="center" sx={{padding:'10px', borderRadius:'5px', border: '1px solid grey', cursor:'pointer'}} onClick={() => handleEditPassenger(passenger)}>
+                {getAvatar(passenger)} 
                 <Box>
-                  <Typography>{passenger.full_name}</Typography>
-                  {passenger.lead_passenger ? <Chip label="Lead Passenger" size="small" color="warning" /> : `Passenger ${index + 1}`}
-                  {passenger.empty ? <><br /><Chip label="available" size="small" color="info" sx={{ color: "white" }} /></> : <></>}
+                  <Typography variant="h6" style={{ fontSize: '0.8rem', fontWeight: '400' }}>{passenger.full_name}</Typography>
+                  {!passenger.empty && (<Chip
+                    label={passenger.lead_passenger ? "Lead Passenger" : `Passenger ${[passenger.passenger_order]}`}
+                    size="small"
+                    color={passenger.lead_passenger ? "warning" : "default"}
+                    sx={{ color: "white" }}
+                  />)}
+                  {passenger.empty == true && (
+                    <>
+                      <Chip label="Available" size="small" color="info" sx={{ color: "white" }} />
+                    </>
+                  )}
                 </Box>
               </Box>
             </Grid>
@@ -175,7 +224,7 @@ const Passengers: React.FC<PassengersProps> = ({ booking, editMode, setLoading }
         editMode={editMode}
         onSave={handleSavePassenger}
         onDelete={onDelete}
-        savingLoading ={savinLoading}
+        savingLoading={savinLoading}
         releaseLoading={releaseLoading}
         isSingleRoom={isSingleRoom}
         onChange={(field, value) =>
