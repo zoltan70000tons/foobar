d295432f1a4875735bba00b47a720ea5dcb8e8af
nicanor-70K-IT <nicanor@70000tons.com>
2025-03-20 18:29:11 -0300

    [chore] first batch of updated translations

---
+++ lang/de/systemErrors.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+  'booking_not_found' => '',
+  'pax_not_found' => '', 
+  'no_adjustments' => '',
+  'unauthorized' => '',
+  'cart_empty' => '',
+  'invalid_expired'   => '', 
+];
