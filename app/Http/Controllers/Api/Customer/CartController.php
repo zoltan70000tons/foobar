142bfd08ad0558eac9b8feca0a648066aaf08648
carlos-70k <carlos@70000tons.com>
2025-04-07 11:54:22 -0700

    Add missing translation, add format to dob

--- app/Http/Controllers/Api/Customer/CartController.php
+++ app/Http/Controllers/Api/Customer/CartController.php
@@ -164,7 +164,7 @@ class CartController extends Controller
       // If age restricion is false return error
       $isAgeValid = AgeRestriction::isAgeValid($dateOfBirth, 21);
       if (!$isAgeValid) {
-        return response()->json(['message' => 'Age restriction not met. Minimum age is 21 years old to proceed.'], 400);
+        return response()->json(['message' => __('feedback.age_restriction')], 400);
       }
     } else {
       return response()->json(['message' => 'Date of birth is required'], 400);
@@ -231,7 +231,7 @@ class CartController extends Controller
       // If age restricion is false return error
       $isAgeValid = AgeRestriction::isAgeValid($dateOfBirth, 21);
       if (!$isAgeValid) {
-        return response()->json(['message' => 'Age restriction not met. Minimum age is 21 years old to proceed.'], 400);
+        return response()->json(['message' => __('feedback.age_restriction')], 400);
       }
     } else {
       return response()->json(['message' => 'Date of birth is required'], 400);
