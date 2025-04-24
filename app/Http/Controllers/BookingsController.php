b765d9a924f85d11dbbaadd2442d76a6177229e8
leo <leonardo@70000tons.com>
2025-04-24 17:35:57 -0400

    Add user filtering to bookings retrieval and UI

--- app/Http/Controllers/BookingsController.php
+++ app/Http/Controllers/BookingsController.php
@@ -715,8 +715,9 @@ class BookingsController extends Controller
     $sortKey = $request->input('sort_key', 'created_at');
     $sortDirection = $request->input('sort_direction', 'desc');
     $tags = array_filter(explode(',', $request->input('tags', '')));
+    $user_ids = $request->input('user_ids', []);
     $bookings = $this->bookingRepository->getByStatus($eventId, $status, $keyword, $perPage, $sortKey,
-        $sortDirection, $tags);
+        $sortDirection, $tags,$user_ids);
 
     return response()->json([
       'data' => $bookings->items(),
