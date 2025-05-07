50e876e15ac6976fab6e1e70ef7db7a3e9d0e2e0
carlos-70k <carlos@70000tons.com>
2025-05-07 16:52:40 -0700

    Attribute fee due date to installment

--- app/Models/Fee.php
+++ app/Models/Fee.php
@@ -18,6 +18,11 @@ class Fee extends Model
     'amount'
   ];
 
+  // Use the 'temp_due_date' attribute to store alternate due date on installment creation
+  protected $appends = ['temp_due_date'];
+  protected $hidden = ['temp_due_date'];
+  public $temp_due_date = null;
+
   protected static function boot()
   {
     parent::boot();
@@ -25,7 +30,12 @@ class Fee extends Model
     static::creating(function ($fee) {});
 
     static::created(function ($fee) {
-      $installment = Installment::create(array('due_date' => now(), 'passenger_id' =>  $fee->passenger_id, 'type' => 'FEE',  'fee_id' => $fee->id));
+      Installment::create([
+        'due_date' => $fee->temp_due_date ?? now(),
+        'passenger_id' => $fee->passenger_id,
+        'type' => 'FEE',
+        'fee_id' => $fee->id,
+      ]);
     });
 
     static::updated(function ($fee) {});
