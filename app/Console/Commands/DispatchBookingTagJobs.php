59f8ab89777533a581fb95d8460a59a8a083b648
leo <leonardo@70000tons.com>
2025-05-07 14:24:02 -0400

    Add AutoTagBookingJob for tagging bookings

---
+++ app/Console/Commands/DispatchBookingTagJobs.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Booking;
+use App\Jobs\AutoTagBookingJob;
+
+class DispatchBookingTagJobs extends Command
+{
+    protected $signature = 'bookings:dispatch-tags';
+    protected $description = 'Dispatches tagging jobs for each booking';
+
+    public function handle()
+    {
+        $bookings = Booking::with(['passengers'])->get();
+
+        foreach ($bookings as $booking) {
+            AutoTagBookingJob::dispatch($booking);
+        }
+
+        $this->info('All tagging jobs dispatched successfully.');
+    }
+}
