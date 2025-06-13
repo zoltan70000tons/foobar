e4cbc4437e98e621ce83948a2361135f3ce79d47
blift70k <jacek@70000tons.com>
2025-06-13 17:01:53 +0200

    Skip on local add bar on top

--- app/Http/Middleware/ElectronAuth.php
+++ app/Http/Middleware/ElectronAuth.php
@@ -1,20 +1,25 @@
 <?php
+
 namespace App\Http\Middleware;
 
+use Illuminate\Support\Facades\App;
 use Closure;
 
 class ElectronAuth
 {
   public function handle($request, Closure $next)
   {
-    // We can add here if environment is local just skip this middleware
+    // if environmt is local, skip the middleware
+    if (App::environment('local')) {
+      return $next($request);
+    }
 
 
     $headerToken = $request->header('X-Device-Token');
     $validToken = env('ADMIN_DEVICE_TOKEN');
 
     if ($headerToken !== $validToken) {
-        abort(403, 'Unauthorized device');
+      abort(403, 'Unauthorized device');
     }
 
     return $next($request);
---
+++ resources/js/Components/EnvironmentBar.tsx
@@ -0,0 +1,33 @@
+import { Box } from "@mui/material";
+
+export default function EnvironmentBar({
+  environment,
+}: {
+  environment: string | null;
+}) {
+
+  if (!environment) {
+    return null;
+  }
+
+  const color = environment === 'stage' ? 'red' : environment === 'prod' ? 'green' : 'gray';
+
+  return (
+    <Box
+      sx={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '20px',
+        backgroundColor: color,
+        color: 'white',
+        textAlign: 'center',
+        zIndex: 9999,
+        fontSize: '12px',
+      }}
+    >
+      {environment.toUpperCase()}
+    </Box>
+  );
+}
