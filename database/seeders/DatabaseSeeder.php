fc8fd5f91faf5c28db3bb9d654a75c4176b34727
blift70k <jacek@70000tons.com>
2025-09-03 15:34:20 +0200

    refactor: Implement UUID for passengers and enhance token management in CheckBookingController.
    Run seeder and migration required

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -35,6 +35,7 @@ class DatabaseSeeder extends Seeder
             CustomerBookingSeeder::class,
             UserTagsSeeder::class,
             OauthClientSeeder::class,
+            PassengerPersonalAccessClientSeeder::class,
         ]);
     }
 }
