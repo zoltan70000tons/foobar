1e2da016ac939ade35be25c2e37038c282a136b3
blift70k <jacek@70000tons.com>
2025-09-05 14:29:26 +0200

    refactor: Update OauthClientSeeder to use dynamic frontend URL for redirect URIs.
    After migration value for redirect_uris should be inserted from FRONTEND_URL env.
    
    Please keep it mind that this is seeder, on production we should create client via passport command

--- database/seeders/OauthClientSeeder.php
+++ database/seeders/OauthClientSeeder.php
@@ -10,6 +10,10 @@ class OauthClientSeeder extends Seeder
 {
     public function run()
     {
+        // Get the frontend URL from config
+        $frontendUrl = config('app.frontend_url');
+        $redirectUri = "{$frontendUrl}/auth/callback";
+
         DB::table('oauth_clients')->insert([
             'id' => '9f79697c-6d55-4148-8edc-111c76fc38f5',
             'owner_type' => null,
@@ -18,7 +22,7 @@ class OauthClientSeeder extends Seeder
             'secret' => null,
             'provider' => null,
             'redirect_uris' => json_encode([
-                "http://localhost:3000/auth/callback"
+                $redirectUri
             ]),
             'grant_types' => json_encode([
                 "authorization_code",
