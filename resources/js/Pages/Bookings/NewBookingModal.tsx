55b5c5ba3c8b56121dd57aad74ec7840f1089e98
blift70k <jacek@70000tons.com>
2025-12-03 17:30:03 +0100

    feat: implement nested modal for creating customers in booking process

--- resources/js/Pages/Bookings/NewBookingModal.tsx
+++ resources/js/Pages/Bookings/NewBookingModal.tsx
@@ -1,54 +1,154 @@
-import React, { useState } from "react";
-import { Button, Dialog, DialogTitle, DialogContent, DialogActions } from "@mui/material";
+import { useState } from "react";
+import { Button, Box, Modal, Paper } from "@mui/material";
 import { usePermissions } from "@/Providers/PermissionContext";
 import { Permissions } from "@/enums/PermissionEnum";
 import BookingStepper from "./BookingStepper";
 import { router } from "@inertiajs/react";
+import CreateCustomer from "../Customer/partials/CreateCustomer";
 
-const NewBookingModal: React.FC = ({cabinTypes, cabinCategories,onBookingCreated}) => {
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  bgcolor: "background.paper",
+  border: "1px solid #3f3f3f",
+  borderRadius: 2,
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+  width: "100%",
+  maxWidth: 1200,
+};
 
+export default function NewBookingModal({ cabinTypes, cabinCategories, onBookingCreated }) {
   const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-  const {hasPermission} = usePermissions(); 
+
+  // open and set param modal in url
+  const handleOpen = () => {
+    router.visit(window.location.pathname, {
+      data: { modal: "new_booking" },
+      preserveScroll: true,
+      preserveState: true,
+    });
+
+    setOpen(true);
+  };
+
+  // close and remove param modal from url
+  const handleClose = () => {
+    router.visit(window.location.pathname, {
+      // Remove modal param from URL
+      data: { modal: null },
+      preserveScroll: true,
+      preserveState: true,
+    });
+    setOpen(false);
+  };
+
+  const { hasPermission } = usePermissions();
   const canCreateBooking = hasPermission(Permissions.CreateBookings);
   const [isCreateCustomerVisible, setIsCreateCustomerVisible] = useState(false);
 
   return (
     <>
-      <Button variant="outlined" color="secondary" onClick={handleOpen} disabled={!canCreateBooking} style={{height:'40px'}}>
+      <Button
+        variant="outlined"
+        color="secondary"
+        onClick={handleOpen}
+        disabled={!canCreateBooking}
+        style={{ height: "40px" }}
+      >
         New Booking
       </Button>
-
-      {/* Modal */}
-      <Dialog open={open} onClose={handleClose} maxWidth="lg" fullWidth>
-        <DialogTitle>New Booking</DialogTitle>
-        <DialogContent>
-          <BookingStepper cabinTypes={cabinTypes} cabinCategories={cabinCategories} close={handleClose} setIsCreateCustomerVisible={setIsCreateCustomerVisible} onBookingCreated={onBookingCreated}/>
-        </DialogContent>
-        <DialogActions>
-          {isCreateCustomerVisible && (
-            <a
-              href={route('customers.create')}
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{ textDecoration: 'none' }}
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="parent-modal-title"
+        aria-describedby="parent-modal-description"
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Box sx={{ ...style }}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: 2,
+            }}
+          >
+            <h2 id="parent-modal-title">New Booking</h2>
+            <BookingStepper
+              cabinTypes={cabinTypes}
+              cabinCategories={cabinCategories}
+              close={handleClose}
+              setIsCreateCustomerVisible={setIsCreateCustomerVisible}
+              onBookingCreated={onBookingCreated}
+            />
+            <Box
+              sx={{
+                display: "flex",
+                gap: 2,
+                justifyContent: "flex-end",
+              }}
             >
-              <Button
-                variant="outlined"
-                color="warning"
-              >
-                Create Customer
+              {isCreateCustomerVisible && <CustomerModal />}
+              <Button onClick={handleClose} variant="outlined" color="secondary">
+                Cancel
               </Button>
-            </a>
-          )}
-          <Button onClick={handleClose} variant="outlined" color="secondary">
-            Cancel
-          </Button>
-        </DialogActions>
-      </Dialog>
+            </Box>
+          </Box>
+        </Box>
+      </Modal>
+    </>
+  );
+}
+
+const CustomerModal = () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    console.log("Opening Customer Modal");
+
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Button variant="outlined" color="warning" onClick={handleOpen}>
+        Create Customer
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="child-modal-title"
+        aria-describedby="child-modal-description"
+      >
+        <Box
+          sx={{
+            ...style,
+            width: 1300,
+            height: "70vh",
+            mt: "auto",
+            overflow: "auto",
+            alignItems: "center",
+            justifyContent: "center",
+
+            py: 0,
+            zIndex: (theme) => theme.zIndex.modal + 1,
+          }}
+        >
+          <Box sx={{ position: "relative" }}>
+            <CreateCustomer handleClose={handleClose} />
+          </Box>
+        </Box>
+      </Modal>
     </>
   );
 };
-
-export default NewBookingModal;
---
+++ resources/js/Pages/Customer/partials/CreateCustomer.tsx
@@ -0,0 +1,396 @@
+import React, { useEffect, useState } from "react";
+import { Head, useForm } from "@inertiajs/react";
+import { PageProps } from "@/types";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { router } from "@inertiajs/react";
+import {
+  Container,
+  Paper,
+  Grid,
+  Toolbar,
+  TextField,
+  Box,
+  Button,
+  Typography,
+  Select,
+  MenuItem,
+  FormHelperText,
+  FormControl,
+  InputLabel,
+} from "@mui/material";
+import { useSnackbar } from "@/Providers/SnackBarAlertProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { Permissions } from "@/enums/PermissionEnum";
+import Country from "@/Components/Country";
+import PhoneNumber from "@/Components/PhoneNumber";
+import { usePermissions } from "@/Providers/PermissionContext";
+import dayjs from "dayjs";
+import iso3166 from "iso-3166-2";
+
+type CountryWithSubs = ReturnType<typeof iso3166.country> & {
+  sub?: Record<string, { type: string; name: string }>;
+};
+
+type Props = {
+  handleClose?: () => void;
+};
+
+export default function CreateCustomer({ handleClose }: Props) {
+  const { hasPermission } = usePermissions();
+
+  const { data, setData, errors, processing } = useForm({
+    email: "",
+    survivor_number: "",
+    first_name: "",
+    last_name: "",
+    middle_name: "",
+    gender: "M",
+    dob: "",
+    citizenship: "",
+    phone: "",
+    address_first: "",
+    address_second: "",
+    city: "",
+    state: "",
+    postal_code: "",
+    country: "",
+    emergency_c_name: "",
+    emergency_c_phone: "",
+    language: "",
+  });
+
+  const { showSnackbar } = useSnackbar();
+
+  const selectedCountry = data.country;
+
+  const getStateOptions = (countryCode: string) => {
+    const country = iso3166.country(countryCode) as CountryWithSubs;
+    const subdivisions = country?.sub || {};
+
+    const seen = new Set<string>();
+
+    return Object.entries(subdivisions)
+      .map(([fullCode, { name }]) => ({ label: name, value: name }))
+      .filter(({ label }) => {
+        if (seen.has(label)) {
+          return false;
+        }
+        seen.add(label);
+        return true;
+      })
+      .sort((a, b) => a.label.localeCompare(b.label));
+  };
+
+  const stateOptions = getStateOptions(selectedCountry);
+
+  useEffect(() => {
+    setData("state", "");
+  }, [selectedCountry]);
+
+  const handleChange = <TForm extends Record<string, unknown>>(
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    const { name, value } = e.target;
+
+    setData(name as keyof TForm, value as TForm[keyof TForm]);
+  };
+
+  const handleSelectChange = (e: React.ChangeEvent<{ name?: string; value: unknown }>) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setData("state", value as string);
+  };
+
+  const handleStringChange = <TForm extends Record<string, unknown>>(value: string, name: keyof TForm) => {
+    setData(name, value as TForm[keyof TForm]);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    for (const key in data) {
+      formData.append(key, data[key]);
+    }
+    router.post(route("customers.store"), formData, {
+      forceFormData: true,
+      onSuccess: (response) => {
+        showSnackbar("Customer created successfully", "success");
+      },
+      onError: (errors) => {
+        const errorMessages = Object.values(errors).join("\n");
+        showSnackbar(`Error creating customer\n${errorMessages}`, "error");
+      },
+    });
+  };
+
+  return (
+    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+      <Grid container spacing={3}>
+        {hasPermission(Permissions.CreateCustomers) && (
+          <Paper
+            sx={{
+              p: 2,
+              display: "flex",
+              flexDirection: "column",
+              minHeight: 240,
+              width: "100%",
+            }}
+          >
+            <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", mb: 2 }}>
+              <h1>Create Customer</h1>
+              {handleClose && (
+                <Button variant="outlined" color="secondary" onClick={handleClose} sx={{ mb: 2 }}>
+                  Close
+                </Button>
+              )}
+            </Box>
+            <form onSubmit={handleSubmit} encType="multipart/form-data">
+              <Box sx={{ width: "100%" }}>
+                <Grid container spacing={2}>
+                  <Grid item xs={6}>
+                    <TextField
+                      fullWidth
+                      label="eMail"
+                      variant="outlined"
+                      value={data.email}
+                      name={"email"}
+                      onChange={handleChange}
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <TextField
+                      fullWidth
+                      label="First Name"
+                      variant="outlined"
+                      value={data.first_name}
+                      name={"first_name"}
+                      onChange={handleChange}
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <TextField
+                      fullWidth
+                      label="Middle Name"
+                      variant="outlined"
+                      name={"middle_name"}
+                      value={data.middle_name}
+                      onChange={handleChange}
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <TextField
+                      fullWidth
+                      label="Last Name"
+                      variant="outlined"
+                      value={data.last_name}
+                      name={"last_name"}
+                      onChange={handleChange}
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Select
+                      fullWidth
+                      label="Gender"
+                      variant="outlined"
+                      value={data.gender}
+                      name={"gender"}
+                      onChange={handleChange}
+                    >
+                      <MenuItem value={"M"}>Male</MenuItem>
+                      <MenuItem value={"F"}>Female</MenuItem>
+                    </Select>
+                  </Grid>
+                </Grid>
+              </Box>
+
+              <Box sx={{ width: "100%", mt: 2, mb: 2 }}>
+                <Grid container spacing={2}>
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <Grid item xs={6}>
+                      <DatePicker
+                        label="Date of Birth"
+                        sx={{ width: "100%" }}
+                        value={data.dob ? dayjs(data.dob) : null}
+                        maxDate={dayjs()} // Restricts future dates
+                        format="YYYY-MM-DD" // Ensures consistent formatting
+                        onChange={(e) => handleStringChange(e, "dob")}
+                        renderInput={(params) => <TextField {...params} fullWidth />}
+                      />
+                    </Grid>
+                  </LocalizationProvider>
+                  <Grid item xs={6}>
+                    <Country
+                      fullWidth
+                      label="Country"
+                      variant="outlined"
+                      value={data.citizenship}
+                      name={"citizenship"}
+                      onChange={(e) => handleStringChange(e, "citizenship")}
+                    />
+                  </Grid>
+                </Grid>
+              </Box>
+
+              <Typography variant="h6" sx={{ mt: 2, mb: 2 }}>
+                Phone Number
+              </Typography>
+              <Box sx={{ width: "100%" }}>
+                <Grid container spacing={2}>
+                  <Grid item xs={6}>
+                    <PhoneNumber
+                      value={data.phone || ""}
+                      forceDialCode={true}
+                      name={"phone"}
+                      onChange={(e) => handleStringChange(e, "phone")}
+                    />
+                  </Grid>
+                </Grid>
+              </Box>
+
+              <Typography variant="h6" sx={{ mt: 2, mb: 2 }}>
+                Address Information
+              </Typography>
+              <Box sx={{ width: "100%" }}>
+                <Grid container spacing={2}>
+                  <Grid item xs={6}>
+                    <TextField
+                      fullWidth
+                      label="Address Line 1"
+                      variant="outlined"
+                      value={data.address_first}
+                      name={"address_first"}
+                      onChange={handleChange}
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <TextField
+                      fullWidth
+                      label="Address Line 2"
+                      variant="outlined"
+                      value={data.address_second}
+                      name={"address_second"}
+                      onChange={handleChange}
+                    />
+                  </Grid>
+
+                  <Grid item xs={4}>
+                    <TextField
+                      fullWidth
+                      label="City"
+                      variant="outlined"
+                      value={data.city}
+                      name={"city"}
+                      onChange={handleChange}
+                    />
+                  </Grid>
+                  <Grid item xs={4}>
+                    <FormControl
+                      required={selectedCountry === "USA" || selectedCountry === "CAN"}
+                      fullWidth
+                      error={!!errors.state}
+                    >
+                      <InputLabel id="state-label">State</InputLabel>
+                      <Select
+                        labelId="state-label"
+                        label="State"
+                        value={data.state}
+                        onChange={handleSelectChange}
+                        variant="outlined"
+                        error={!!errors.state}
+                      >
+                        {stateOptions.map(({ value, label }) => (
+                          <MenuItem key={value} value={value}>
+                            {label}
+                          </MenuItem>
+                        ))}
+                      </Select>
+                      <FormHelperText>{errors.state}</FormHelperText>
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={4}>
+                    <TextField
+                      fullWidth
+                      label="Zip Code"
+                      variant="outlined"
+                      value={data.postal_code}
+                      name={"postal_code"}
+                      onChange={handleChange}
+                    />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <Country
+                      fullWidth
+                      label="Country"
+                      variant="outlined"
+                      value={data.country}
+                      name={"country"}
+                      onChange={(e) => handleStringChange(e, "country")}
+                    />
+                  </Grid>
+                </Grid>
+              </Box>
+
+              <Typography variant="h6" sx={{ mt: 2, mb: 2 }}>
+                Emergency Contact
+              </Typography>
+              <Box sx={{ width: "100%" }}>
+                <Grid container spacing={2}>
+                  <Grid item xs={6}>
+                    <TextField
+                      fullWidth
+                      label="Contact Full Name"
+                      variant="outlined"
+                      value={data.emergency_c_name}
+                      name={"emergency_c_name"}
+                      onChange={handleChange}
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <PhoneNumber
+                      value={data.emergency_c_phone || ""}
+                      forceDialCode={true}
+                      name={"emergency_c_phone"}
+                      onChange={(e) => handleStringChange(e, "emergency_c_phone")}
+                    />
+                  </Grid>
+                </Grid>
+              </Box>
+
+              <Typography variant="h6" sx={{ mt: 2, mb: 2 }}>
+                Preferred Language
+              </Typography>
+              <Box sx={{ width: "100%" }}>
+                <Grid container spacing={2}>
+                  <Grid item xs={6}>
+                    <Select
+                      fullWidth
+                      label="Preferred Language"
+                      variant="outlined"
+                      value={data.language}
+                      name={"language"}
+                      onChange={handleChange}
+                    >
+                      <MenuItem value={"de"}>Deutsch</MenuItem>
+                      <MenuItem value={"en"}>English</MenuItem>
+                      <MenuItem value={"es"}>Español</MenuItem>
+                    </Select>
+                  </Grid>
+                </Grid>
+              </Box>
+              <Box sx={{ mt: 4 }}>
+                <Button variant="contained" color="primary" fullWidth type="submit" disabled={processing}>
+                  {processing ? "Submitting..." : "Submit"}
+                </Button>
+              </Box>
+            </form>
+          </Paper>
+        )}
+      </Grid>
+    </Container>
+  );
+}
